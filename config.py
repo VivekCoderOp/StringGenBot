7dd4309bbb6b5b3ab8580a9d3261a386d99437d5
Vivek <123234657+VivekCoderOp@users.noreply.github.com>
2023-10-01 02:20:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
-OWNER_ID = int(getenv("OWNER_ID", ""))
+OWNER_ID = int(getenv("OWNER_ID", "6654918132"))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NoobxCreator")
